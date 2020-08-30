f15b3e4a2ff7b39bd938f15179e9febae8677e50
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-30 21:42:44 +0700

    default values on destructuring object

--- destructuring/basics.js
+++ destructuring/basics.js
@@ -12,7 +12,23 @@ const person = {
 // const name = person.name
 // const snack = person.snack
 
-// new
-const { name: myName, snack }= person
+// new ways
+const { 
+    name: myName = 'Sul', 
+    snack,
+    cat,
+    cat: {
+        name
+    },
+    // jika object cat2 tidak ada dalam object utama, bisa dibuat default.
+    // cat2: {
+    //     name_
+    // } = {
+    //     name_: 'cat23'
+    // }
+}= person
 console.log(myName);
 console.log(snack);
+console.log(name);
+
+// [Array Destructuring]
