b32914672c5a03599967eaabf7fbb37b66d5298e
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-10-25 08:20:25 +0700

    Add Routes

--- express-beginner/index.js
+++ express-beginner/index.js
@@ -3,3 +3,8 @@ const app = express();
 
 const port = 3000;
 app.listen(port, () => console.log(`Server Run On Port ${port}`));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
