e398bd6a0df3543d3ee32ed4d30a0cee26aded76
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-24 21:23:37 +0700

    + add hooks mongoose

--- node-jwt-ninjas/src/models/User.js
+++ node-jwt-ninjas/src/models/User.js
@@ -17,6 +17,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created & save', doc);
+    next();
+})
+
+// Fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    console.log('user about tobe created & save', this);
+    next();
+})
+
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
