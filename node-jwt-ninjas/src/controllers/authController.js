0258d986091d40e8424e972419cc0b5072409a80
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-24 20:27:27 +0700

    ^ update auth error handling

--- node-jwt-ninjas/src/controllers/authController.js
+++ node-jwt-ninjas/src/controllers/authController.js
@@ -1,3 +1,26 @@
+const User = require("../models/User");
+
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    const errors = {
+        email: "",
+        password: ""
+    }
+
+    if (err.code === 11000) {
+        errors.email = "That email has been registered.";
+        return errors;
+    }
+
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,10 +29,22 @@ module.exports.signin_get = (req, res) => {
     res.render('signin');
 }
 
-module.exports.signup_post = (req, res) => {
-    res.send('new signup');
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({
+            email, password
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        const errors = handleErrors(error)
+        res.status(400).json(errors);
+    }
 }
 
-module.exports.signin_post = (req, res) => {
+module.exports.signin_post = async (req, res) => {
+    const { email, password } = req.body;
     res.send('user login');
 }
