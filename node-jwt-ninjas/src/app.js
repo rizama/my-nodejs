19313d5b3276fae0d20baee133e07159ec88b5d7
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-25 22:12:53 +0700

    ^ Cookies Primer

--- node-jwt-ninjas/src/app.js
+++ node-jwt-ninjas/src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const path = require('path');
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 require('dotenv').config();
 
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 // view engine
 app.set('views', path.join(__dirname, 'views'))
@@ -34,6 +36,22 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+app.get('/set-cookies', (req, res) => {
+    
+    // use cookie-parser as a Middleware
+    res.cookie("newUser", false);
+    res.cookie("isEmployee", true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+    res.send("Your cookies is created")
+})
+
+app.get('/read-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+
+    res.json(cookies)
+})
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
