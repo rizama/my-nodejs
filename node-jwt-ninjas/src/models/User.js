b068f0864aab2a6191f35909d19bd3f265bf2b1a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-27 21:25:21 +0700

    + add statics method in User Model

--- node-jwt-ninjas/src/models/User.js
+++ node-jwt-ninjas/src/models/User.js
@@ -23,7 +23,22 @@ userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
-})
+});
+
+// static method to login user
+userSchema.statics.signin = async function (email, pass) {
+    const user = await this.findOne({ email });
+
+    if (user) {
+        const auth = await bcrypt.compare(pass, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('Incorrect Password!')
+    }
+
+    throw Error('Incorrect Email!');
+};
 
 const User = mongoose.model('user', userSchema);
 
