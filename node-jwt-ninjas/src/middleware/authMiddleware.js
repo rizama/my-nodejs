651cffb340444c10b6411becc9b4e45f009ce0e8
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-28 21:56:01 +0700

    ^ update return

--- node-jwt-ninjas/src/middleware/authMiddleware.js
+++ node-jwt-ninjas/src/middleware/authMiddleware.js
@@ -6,14 +6,14 @@ module.exports = {
 
         // check cookie exist or not
         if (!cookie) {
-            res.redirect('/signin');
+            return res.redirect('/signin');
         }
 
         // verify jwt
         jwt.verify(cookie, process.env.SECRET_STRING, (err, decodedToken) => {
             if (err) {
                 console.log(err.message);
-                res.redirect('/signin');
+                return res.redirect('/signin');
             } else {
                 console.log(decodedToken);
                 next();
