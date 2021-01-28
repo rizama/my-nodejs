94e683501282e632c604b0bee4867932007b3008
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-28 21:55:49 +0700

    ^ add routes

--- node-jwt-ninjas/src/routes/authRoutes.js
+++ node-jwt-ninjas/src/routes/authRoutes.js
@@ -7,5 +7,6 @@ router.get('/signup', authController.signup_get);
 router.post('/signup', authController.signup_post);
 router.get('/signin', authController.signin_get);
 router.post('/signin', authController.signin_post);
+router.get('/signout', authController.signout_get);
 
 module.exports = router;
