967899989c2e373dfe77bcdfb17968a863c76f4f
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-02 22:39:56 +0700

    ^ add client id and secret

--- node-oauth/src/config/passportSetup.js
+++ node-oauth/src/config/passportSetup.js
@@ -1,9 +1,12 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 
+
 passport.use(
     new GoogleStrategy({
         // option for the strategy
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
     }), () => {
         // passport callback
     }
