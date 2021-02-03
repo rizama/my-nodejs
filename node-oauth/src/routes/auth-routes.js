5f62ea73c998b18918d79fcbd6bf718364ab2c68
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-03 22:52:01 +0700

    ^ add middleware passport

--- node-oauth/src/routes/auth-routes.js
+++ node-oauth/src/routes/auth-routes.js
@@ -18,7 +18,7 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
 
-router.get('/google/redirect', (req, res) => {
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
     res.send("You Reached the callback URI");
 });
 
