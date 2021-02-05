f0d06a4f9b0ac694b78cbc7ad154d2a3378ee780
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-05 23:16:13 +0700

    ^ retrieving user

--- node-oauth/src/config/passportSetup.js
+++ node-oauth/src/config/passportSetup.js
@@ -9,14 +9,20 @@ passport.use(
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET
     }, async (accessToken, refreshToken, profile, done) => {
-        // passport callback
-        console.log(profile);
-        const user = await User.create({
-            username: profile.displayName,
-            googleId: profile.id,
-            email: profile._json.email
-        })
+        // Check if User Exist in out DB
+        let currentUser = await User.findOne({
+            googleId: profile.id
+        });
 
-        console.log(user);
+        if (currentUser) {
+            console.log("user is", currentUser);
+        } else {
+            const user = await User.create({
+                username: profile.displayName,
+                googleId: profile.id,
+                email: profile._json.email
+            });
+        }
+
     })
 );
