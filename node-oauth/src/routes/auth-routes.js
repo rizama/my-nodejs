356dc556e9390e4e45663e7d48d8c4870146cbfc
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-07 21:12:13 +0700

    ^ call user data authenticated

--- node-oauth/src/routes/auth-routes.js
+++ node-oauth/src/routes/auth-routes.js
@@ -19,7 +19,7 @@ router.get('/google', passport.authenticate('google', {
 }));
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send("You Reached the callback URI");
+    res.send(req.user);
 });
 
 module.exports = router;
