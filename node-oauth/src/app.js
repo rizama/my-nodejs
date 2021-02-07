5f7e713c8bc6d499d9fdd32a6d6bd9f81abd839c
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-07 21:23:04 +0700

    ^ apply redirecting user

--- node-oauth/src/app.js
+++ node-oauth/src/app.js
@@ -4,8 +4,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
 const authRoute = require("./routes/auth-routes");
+const profileRoutes = require('./routes/profile-routes');
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
 
 require('dotenv').config();
@@ -22,9 +23,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser({
+app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000,
-  key: [process.env.KEY_SESSION_COOKIE]
+  keys: [process.env.KEY_SESSION_COOKIE]
 }));
 
 // view engine
@@ -42,6 +43,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((e) => console.log(e));
 
 app.use('/auth', authRoute);
+app.use('/profile', profileRoutes);
+
 app.get('/', (req, res) => {
   res.render('home');
 });
