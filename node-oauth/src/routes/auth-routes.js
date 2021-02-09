b2c6da6612c592c5106898a8bad7e965a30c783a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-09 21:41:13 +0700

    ^ add jwt in cookies and logout

--- node-oauth/src/routes/auth-routes.js
+++ node-oauth/src/routes/auth-routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 // auth login
 router.get('/login', (req, res) => {
@@ -11,6 +12,10 @@ router.get('/login', (req, res) => {
 router.get('/logout', (req, res) => {
     // handle with passport
     req.logout();
+    
+    // remove JWT
+    res.cookie('jwt', '', { maxAge: 1 });
+    
     res.redirect("/");
 });
 
@@ -19,8 +24,16 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
+const maxAge = 1 * 24 * 60 * 60; //1 days in seconds
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.KEY_JWT, {
+        expiresIn: maxAge
+    });
+};
+
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    // res.send(req.user);
+    const token = createToken(req.user.id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.redirect('/profile');
 });
 
