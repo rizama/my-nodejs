041f8ceded49e93d1b44a34aa83fae2b5305e059
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-15 09:08:13 +0700

    ^ Basic One

--- telegram-bot/src/app.js
+++ telegram-bot/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const { Telegraf } = require('telegraf');
 
 require('dotenv').config();
 
@@ -15,6 +16,36 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Constructor 
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Important Commands
+bot.start((ctx) => ctx.reply('Hallo Bos!'));
+bot.help((ctx) => {
+  ctx.reply("Ada yang bisa dibantu?");
+});
+bot.settings((ctx) => {
+  ctx.reply("Ini Setting");
+});
+
+// Telegraf Context
+// bot.start((ctx) => ctx.reply(`Hallo ${ctx.from.first_name}`));
+
+// BOT Command Method
+bot.command(['halo', 'hola'], (ctx) => {
+  ctx.reply("Command Accepted");
+});
+
+
+// BOT Hears Method
+bot.hears('Hai Sam', (ctx) => {
+  ctx.reply("hai juga");
+});
+
+
+
+bot.launch();
 
 app.get('/', (req, res) => {
   res.json({
