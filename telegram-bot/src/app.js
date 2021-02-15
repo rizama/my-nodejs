e5c30f3ea99e8504acd5338f6dfe595b972ed015
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-15 21:55:15 +0700

    ^ Basic Two

--- telegram-bot/src/app.js
+++ telegram-bot/src/app.js
@@ -20,8 +20,16 @@ app.use(express.urlencoded({ extended: true }));
 // Constructor 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// State
+bot.use((ctx, next) => {
+  ctx.state.buah = 9;
+  ctx.reply("middleware here");
+  next();
+});
+
 // Important Commands
-bot.start((ctx) => ctx.reply('Hallo Bos!'));
+bot.start((ctx) => ctx.reply(`Hallo Bos! ${ctx.state.buah}`));
+
 bot.help((ctx) => {
   ctx.reply("Ada yang bisa dibantu?");
 });
@@ -43,7 +51,30 @@ bot.hears('Hai Sam', (ctx) => {
   ctx.reply("hai juga");
 });
 
+// BOT on Method
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+// bot.on('text', (ctx) => ctx.reply('Text 👍'));
 
+// Other Method
+bot.mention('rizkysamp', (ctx) => {
+  ctx.reply("samsudin");
+});
+
+bot.phone('+62 85659122410', (ctx) => {
+  ctx.reply("My Nomor");
+});
+
+// BOT use Method
+// bot.use((ctx) => {
+//   ctx.state.buah = 9;
+//   ctx.reply("middleware here");
+// });
+
+// Next Function
+// bot.use((ctx, next) => {
+//   ctx.reply("middleware here");
+//   next();
+// });
 
 bot.launch();
 
