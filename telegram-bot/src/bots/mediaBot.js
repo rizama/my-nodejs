ed25b7a60f0f52e1592f51cf274d98a82b020800
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-17 21:33:21 +0700

    ^ Working With File

--- telegram-bot/src/bots/mediaBot.js
+++ telegram-bot/src/bots/mediaBot.js
@@ -1,4 +1,6 @@
 const { Telegraf } = require("telegraf");
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -14,5 +16,20 @@ Referensi Help :
 bot.command(['start', 'help'], (ctx) => {
     ctx.reply(helpMessage);
 });
+
+bot.command('send_photo', ctx => {
+    // Send With URL
+    bot.telegram.sendPhoto(ctx.chat.id, 'https://avatars.githubusercontent.com/u/26889811?s=460&u=c96d60184a889b95b5dd1d34d53c36a5d42fe22b&v=4');
+
+    // Send With Path
+    bot.telegram.sendPhoto(ctx.chat.id, { source: `${appDir}/res/bandung.jpg` });
+
+    // Send With File id
+    bot.telegram.sendPhoto(ctx.chat.id, "AgACAgUAAxkBAAO3YC0oUnc15neXHW96ogX1SqCoxNAAAkuvMRuif2hV_ZbsaWUtLY5aOSFtdAADAQADAgADbQADdJQDAAEeBA");
+});
+
+// bot.on('message', ctx => {
+//     console.log(ctx.message);
+// })
 
 bot.launch();
