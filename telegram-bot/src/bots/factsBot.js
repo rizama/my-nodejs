832a52172b8d70e8c91afdddcee3633379e5e92a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-19 21:54:57 +0700

    ^update fetch data

--- telegram-bot/src/bots/factsBot.js
+++ telegram-bot/src/bots/factsBot.js
@@ -16,3 +16,21 @@ bot.help(ctx => {
     ctx.reply(helpMessage);
 });
 
+getData();
+
+async function getData() {
+    try {
+        let data = await axiosGet('https://spreadsheets.google.com/feeds/cells/1Z_rLZJH7meoYIW4EpCRb24Y8DbW0bXnLCTlMgHVREUA/1/public/full?alt=json');
+        let facts = data.data.feed.entry;
+
+        facts.forEach(fact => {
+            storeData.push({
+                row: fact.gs$cell.row,
+                col: fact.gs$cell.col,
+                val: fact.gs$cell.inputValue,
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
