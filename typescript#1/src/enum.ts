93c29e24df2170f42c405cf9a55788404e3aa236
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-01 20:43:55 +0700

    ^enum

---
+++ typescript#1/src/enum.ts
@@ -0,0 +1,21 @@
+//
+
+// numeric enums
+enum Month {
+	JAN = 1,
+	FEB,
+	MAR,
+	APR,
+	MAY,
+}
+// console.log(Month);
+
+// string enums
+enum Month2 {
+	JAN = "Januari",
+	FEB = "Februari",
+	MAR = "Maret",
+	APR = "April",
+	MAY = "Mei",
+}
+console.log(Month2)
