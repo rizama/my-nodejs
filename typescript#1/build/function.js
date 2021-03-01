9c9db4f35fef9c6d03c4a513986973ca5d3afb38
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-01 20:50:29 +0700

    + function

---
+++ typescript#1/build/function.js
@@ -0,0 +1,11 @@
+"use strict";
+// Tipe data balikan function
+function getName() {
+    return "Hai Aku Sam";
+}
+function calculate(params) {
+    return params * 2;
+}
+function print(message) {
+    console.log(message);
+}
---
+++ typescript#1/src/function.ts
@@ -0,0 +1,13 @@
+// Tipe data balikan function
+
+function getName(): string {
+	return "Hai Aku Sam";
+}
+
+function calculate(params: number): number {
+	return params * 2;
+}
+
+function print(message: string): void {
+	console.log(message);
+}
