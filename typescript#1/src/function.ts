93769f3633790d20b2ba126a14c7295f40061993
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-02 20:37:27 +0700

    ^function as a type

--- typescript#1/src/function.ts
+++ typescript#1/src/function.ts
@@ -11,3 +11,12 @@ function calculate(params: number): number {
 function print(message: string): void {
 	console.log(message);
 }
+
+// Function as Type
+type Age = number;
+let age: Age = 20;
+
+type Tambah = (val1: number, val2: number) => number;
+const Add: Tambah = (val1: number, val2: number) => {
+	return val1 + val2;
+};
