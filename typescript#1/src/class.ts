cf1e79435fe20ee51184a24c39bc9bfbd035a3ab
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-03-04 18:56:43 +0700

    ^inheritance

--- typescript#1/src/class.ts
+++ typescript#1/src/class.ts
@@ -8,11 +8,48 @@
 
 // OR
 
+// class User {
+// 	constructor(public name: string) {
+// 		this.name = name;
+// 	}
+// }
+
+// let user = new User("Sam");
+// console.log(user);
+
+// Inheritance
 class User {
-	constructor(public name: string) {
+	constructor(public name: string, public age: number) {
 		this.name = name;
+		this.age = age;
+	}
+
+	setName(value: string): void {
+		this.name = value;
+	}
+
+	getName = (): string => {
+		return this.name;
+	};
+}
+
+// public 		= bisa di akses disemua class / dari luar class
+// protected	= hanya bisa di akses dari class tersebut dan class turunannya
+// private		= hanya bisa di akses dari class itu sendiri
+
+class Admin extends User {
+	read: boolean = true;
+	write: boolean = true;
+
+	getRole(): { read: boolean; write: boolean } {
+		return {
+			read: this.read,
+			write: this.write,
+		};
 	}
 }
 
-let user = new User("Sam");
-console.log(user);
+let admin = new Admin("sam", 25);
+admin.getName();
+admin.getRole();
+admin.setName("samsudin");
